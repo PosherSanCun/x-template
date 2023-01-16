bbea0c7618d0ba7cda7ef6ed00de10d9d7f3d8e7
Xavier Zheng <Xavier_CHN@Live.com>
2023-01-16 10:23:43 +0800

    Update script.tsx

--- content/panorama/src/hud/script.tsx
+++ content/panorama/src/hud/script.tsx
@@ -2,10 +2,13 @@ import '@demon673/panorama-polyfill/lib/console';
 import '@demon673/panorama-polyfill/lib/timers';
 
 import React from 'react';
-import { render } from '@demon673/react-panorama';
+import { render, useGameEvent } from '@demon673/react-panorama';
 import { useXNetTableKey } from '../hooks/useXNetTable';
 
 const Test: React.FC = () => {
+    useGameEvent(`test_event`, () => {
+        console.log(`test_event`);
+    });
     const [data] = useXNetTableKey(`test_table`, `test_key`, { data_1: `unknown` });
     const string_data = data.data_1;
     return React.useMemo(() => <Label text={`${string_data}`} />, [string_data]);
@@ -14,3 +17,7 @@ const Test: React.FC = () => {
 render(<Test />, $.GetContextPanel());
 
 console.log(`Hello, world!`);
+
+setInterval(() => {
+    GameEvents.SendCustomGameEventToAllClients(`test_event`, {} as never);
+}, 1000);
